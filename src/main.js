2687edd58285e515d5c62a27a510e7ca81b10574
andreirabon <exilekengkoy@gmail.com>
2025-01-30 01:13:51 +0800

    Additional improvement on design using tailwindcss.

--- src/main.js
+++ src/main.js
@@ -98,7 +98,7 @@ document.querySelector("#navbar").innerHTML = `
 					class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
 					aria-hidden="true">
 					<div
-						class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#48c9b0] to-[##48c9b0] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+						class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#48c9b0] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
 						style="
 							clip-path: polygon(
 								74.1% 44.1%,
@@ -131,25 +131,27 @@ document.querySelector("#navbar").innerHTML = `
 					</h1>
 					<div class="text-center">
 						<h2
-							class="text-4xl font-semibold tracking-tight text-balance text-gray-900 dark:text-gray-100 sm:text-6xl font-inter1">
+							class="text-4xl font-semibold tracking-tight text-balance text-gray-900 dark:text-gray-100 sm:text-6xl font-inter">
 							a Web developer based in the Philippines.
 						</h2>
 						<p
-							class="mt-6 text-justify text-lg font-medium text-pretty text-gray-500 dark:text-gray-400 sm:text-xl/8 max-w-prose mx-auto font-inter2">
+							class="mt-6 text-justify text-lg font-medium text-pretty text-gray-500 dark:text-gray-400 sm:text-xl/8 max-w-prose mx-auto font-inter">
 							With three years of dedicated experience as a
 							<span class="font-semibold">Quality Assurance Tester</span>. I'm
-							transitioning back to my foundational role as a
-							<span class="font-semibold">Web Developer</span>, focusing on
-							<span class="font-semibold">React</span> to develop web
-							applications.
+							now refocusing on my primary expertise as a
+							<span class="font-semibold">Web Developer</span>, specializing in
+							<span class="font-semibold">React</span>. This transition enables
+							me to leverage my background in quality assurance in conjunction
+							with my development skills to create efficient and effective
+							single-page applications.
 						</p>
 					</div>
 				</div>
-				<!-- <div
+				<div
 					class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
 					aria-hidden="true">
 					<div
-						class="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#48c9b0] to-[#48c9b0] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+						class="relative left-[calc(50%+3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-tr from-[#48c9b0] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
 						style="
 							clip-path: polygon(
 								74.1% 44.1%,
@@ -170,7 +172,7 @@ document.querySelector("#navbar").innerHTML = `
 								74.1% 44.1%
 							);
 						"></div>
-				</div> -->
+				</div>
 			</div>
 		</div>
 `;
