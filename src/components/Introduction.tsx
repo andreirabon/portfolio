05b45cebb91d9c24883b25410a89d70bc1f39dff
andreirabon <exilekengkoy@gmail.com>
2025-02-09 12:20:01 +0800

    Version 2 - Almost complete > Minor adjusments.

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -55,7 +55,7 @@ function Introduction() {
 					<Button
 						variant="outline"
 						className="inter group px-6 py-2 text-[#86EFAC] border-[#86EFAC] dark:hover:bg-[#86EFAC] dark:hover:text-black transition-all"
-						onClick={() => window.open("/CV.pdf", "_blank")}>
+						onClick={() => window.open("Andrei R. Rabon - Curriculum Vitae (CV).pdf", "_blank")}>
 						<CgNotes className="mr-2 group-hover:scale-110 transition-transform" />
 						<span>Resume</span>
 					</Button>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,14 +2,15 @@ import { Badge } from "@/components/ui/badge";
 
 function Skills() {
 	const skills = ["HTML", "CSS", "Tailwind CSS", "JavaScript", "TypeScript"];
-	const learning = ["React", "Next.js"];
+	const learning = ["React", "Next.js", "tRPC", "Sass"];
+	const plannedLearning = ["Docker", "Kubernetes", "Terraform", "Go", "Python"];
 
 	return (
 		<>
 			<div
 				id="skills"
 				className="space-y-4">
-				<h1 className="inter text-2xl font-bold tracking-wide dark:text-gray-100">Tech Stack</h1>
+				<h1 className="inter text-2xl font-bold tracking-wide dark:text-gray-100">Skills</h1>
 				<div className="flex flex-wrap gap-2">
 					{skills.map((skill) => (
 						<Badge
@@ -35,6 +36,24 @@ function Skills() {
 							variant="secondary"
 							className="inter px-3 py-1 text-sm font-medium transition-all hover:scale-105 dark:bg-gray-800">
 							{learning}
+						</Badge>
+					))}
+				</div>
+			</div>
+			<br />
+			<div
+				id="skills"
+				className="space-y-4">
+				<h1 className="inter text-2xl font-bold tracking-wide dark:text-gray-100">
+					Future Learning Goals
+				</h1>
+				<div className="flex flex-wrap gap-2">
+					{plannedLearning.map((plannedLearning) => (
+						<Badge
+							key={plannedLearning}
+							variant="secondary"
+							className="inter px-3 py-1 text-sm font-medium transition-all hover:scale-105 dark:bg-gray-800">
+							{plannedLearning}
 						</Badge>
 					))}
 				</div>
