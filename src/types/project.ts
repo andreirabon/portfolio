d14f098fc00092209f65760f78a23baabc4291aa
andreirabon <exilekengkoy@gmail.com>
2025-03-28 00:07:41 +0800

    Improvements.

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,24 +1,29 @@
 interface Project {
-	id: number;
-	picture: {
-		src: string;
-		width: number;
-		height: number;
-		loading: "lazy" | "eager";
-		sizes: string;
-	};
-	pictureAlt: string;
-	title: string;
-	description: string;
-	link: string;
-	tools: string[];
+  id: number;
+  picture: {
+    src: string;
+    width: number;
+    height: number;
+    sizes: string;
+  };
+  pictureAlt: string;
+  title: string;
+  description: string;
+  link: string;
+  tools: string[];
 }
 
 interface PlannedProject {
-	id: number;
-	status: "✅" | "❌";
-	name: string;
-	description: string;
+  id: number;
+  status: "✅" | "❌";
+  name: string;
+  description: string;
+}
+
+export interface ProjectImage {
+  src: string;
+  alt: string;
+  loading?: "lazy" | "eager";
 }
 
 export type { PlannedProject, Project };
